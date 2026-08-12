f9fb63637a13cf5762fd8e58fe0486cb697c38d8
ymzhang0 <zhangyiming1998@gmail.com>
2026-08-12 11:18:48 +0200

    update visualization function export

--- aiida_analyser/visualization/__init__.py
+++ aiida_analyser/visualization/__init__.py
@@ -1,1 +1,22 @@
 """Visualisation helpers for analyser results."""
+from .plots import (
+    plot_eldos,
+    plot_phdos,
+    plot_a2f,
+    plot_aniso,
+    plot_bands,
+    plot_epw_interpolated_bands,
+    plot_iso_gap_function,
+    plot_aniso_gap_function
+)
+
+__all__ = [
+    'plot_eldos',
+    'plot_phdos',
+    'plot_a2f',
+    'plot_aniso',
+    'plot_bands',
+    'plot_epw_interpolated_bands',
+    'plot_iso_gap_function',
+    'plot_aniso_gap_function',
+]
